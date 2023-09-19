24db7c4ae523116a6c02b6cd033e4115d54f377f
schnalzenberger0502 <i.schnalzenberger@students.htl-leonding.ac.at>
2023-09-19 09:38:34 +0200

    fixed more

--- hello/src/main/java/at/htlleonding/App.java
+++ hello/src/main/java/at/htlleonding/App.java
@@ -4,11 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "Meow :3" );
-=======
         System.out.println( "Hello Christi");
->>>>>>> refs/remotes/origin/main
         System.out.println("Hi Isabel ^^");
     }
 }
