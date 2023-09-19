cd693a7eeee85c4253e2154ddd7698968ce32a4a
GitChrisHTL <c.ursprung@HTBLALeonding.onmicrosoft.com>
2023-09-19 09:26:42 +0200

    meow :3

--- hello/src/main/java/at/htlleonding/App.java
+++ hello/src/main/java/at/htlleonding/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Meow :3" );
         System.out.println("Hi Isabel ^^");
     }
 }
